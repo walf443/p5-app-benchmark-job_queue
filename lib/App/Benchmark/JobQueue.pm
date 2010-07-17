0d38c0d25d1ab7fe6e8ed7946e11bb4a23e9eade
Keiji, Yoshimi <walf443@gmail.com>
2010-07-17 22:43:27 +0900

    initial commit

---
+++ lib/App/Benchmark/JobQueue.pm
@@ -0,0 +1,33 @@
+package App::Benchmark::JobQueue;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::Benchmark::JobQueue -
+
+=head1 SYNOPSIS
+
+  use App::Benchmark::JobQueue;
+
+=head1 DESCRIPTION
+
+App::Benchmark::JobQueue is
+
+=head1 AUTHOR
+
+Keiji Yoshimi E<lt>walf443 at gmail dot comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
